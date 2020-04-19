1931d3367d4cc32f880fed03ae3579134b0c6d8d
Gary Kim <gary@garykim.dev>
2020-04-19 14:47:05 +0800

    Add leaderboard functionality

--- backend-src/api/user/index.js
+++ backend-src/api/user/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 import getAPI from './get';
 import createAPI from './create';
+import leaderboardAPI from './leaderboard';
 
 // Accessible from /api/user
 const router = express.Router();
@@ -11,5 +12,8 @@ router.get('/get/:uuid', getAPI);
 
 // Create user /api/user/create
 router.post('/create', createAPI);
+
+// Leaderboard /api/user/leaderboard
+router.get('/leaderboard', leaderboardAPI);
 
 export default router;
