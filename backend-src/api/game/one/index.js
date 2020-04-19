00e789636226782c5f832aa0e1ea578701e386c0
Gary Kim <gary@garykim.dev>
2020-04-20 00:07:47 +0800

    Add game submitdecode API

--- backend-src/api/game/one/index.js
+++ backend-src/api/game/one/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 
 import submitPhraseAPI from './submitphrase';
 import getDecodeAPI from './getDecode';
+import submitDecodeAPI from './submitDecode';
 
 // Accessible from /api/game/one
 const router = express.Router();
 
 router.get('/:game_id/getdecode', getDecodeAPI);
 router.post('/:game_id/submitphrase', submitPhraseAPI);
+router.post('/:game_id/:phrase_id/submitdecode', submitDecodeAPI);
 
 export default router;
