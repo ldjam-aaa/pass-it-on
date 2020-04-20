b88722960e7456fdc3af206bd7049b4f401658cb
Alan C <tcode2k16@gmail.com>
2020-04-20 22:53:28 +0800

    remove the join endpoint

--- backend-src/api/game/index.js
+++ backend-src/api/game/index.js
@@ -5,14 +5,12 @@ import oneAPI from './one/index';
 import listAllAPI from './listall';
 import createAPI from './create';
 import getGameAPI from './getGame';
-import joinAPI from './join';
 
 // Accessible from /api/game
 const router = express.Router();
 
 router.get('/listall', listAllAPI);
 router.post('/create', createAPI);
-router.get('/join', joinAPI);
 // Post request because it may create a new game if necessary.
 router.post('/getgame', getGameAPI);
 
